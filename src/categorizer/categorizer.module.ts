29f3a0f0a93fe44963c73668a24dfee0a6250b86
Danila Kovalev <kovallll.e@gmail.com>
2026-02-17 23:44:37 +0300

    fix: fix module

--- src/categorizer/categorizer.module.ts
+++ src/categorizer/categorizer.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
-
+import { HttpModule } from '@nestjs/axios';
 import { CategorizerService } from './categorizer.service';
 import { CategorizerController } from './categorizer.controller';
 
 @Module({
+  imports: [
+    HttpModule.register({
+      timeout: 10000,
+      maxRedirects: 5,
+    }),
+  ],
   controllers: [CategorizerController],
   providers: [CategorizerService],
   exports: [CategorizerService],
