ed3ad99531b720118f1f84acfe8d12333935228b
Danila Kovalev <kovallll.e@gmail.com>
2026-02-21 13:11:23 +0300

    feat: update pg module

--- src/pg/pg.module.ts
+++ src/pg/pg.module.ts
@@ -9,13 +9,24 @@ export const PG_POOL = 'PG_POOL';
     {
       provide: PG_POOL,
       useFactory: async () => {
+        if (!process.env.DATABASE_URL) {
+          throw new Error('КРИТИЧЕСКАЯ ОШИБКА: DATABASE_URL не задан в process.env!');
+        }
+
+        const dbUrl = new URL(process.env.DATABASE_URL);
+
         const pool = new Pool({
-          connectionString: process.env.DATABASE_URL,
+          host: dbUrl.hostname,
+          port: parseInt(dbUrl.port || '5432', 10),
+          user: dbUrl.username,
+          password: dbUrl.password,
+          database: dbUrl.pathname.replace('/', ''),
           max: 10,
           ssl: {
             rejectUnauthorized: false,
           },
         });
+
         await pool.query('SELECT 1');
         return pool;
       },
